1c35ad7bc3b23f2a9a05a03965e76e2410dc4afc
김성인 <kimsungin5@gmail.com>
2023-07-25 15:34:53 +0900

    feat: swagger 추가
    /swagger-ui로 path로 접속 가능

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.backend.domain.user.dto.UserSignUpDto;
 import com.ssafy.backend.domain.user.service.UserService;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "회원 API", description = "카카오톡 로그인 회원 API")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -19,11 +24,14 @@ public class UserController {
 	private final UserService userService;
 	// private final JwtService jwtService;
 
+	// @Operation(summary = "임시 리다이렉트 페이지")
 	@GetMapping("/oauth2/sign-up")
 	public String signUpForm() {
-		return "sign-up";
+		return "임시 리다이렉트 페이지";
 	}
 
+	@Operation(summary = "추가 정보 받기", description = "카카오톡 로그인 후 필요한 개인정보를 입력받습니다.")
+	@Parameter(name = "userSignDto", description = "회원가입이 추가로 필요한 개인정보 Dto")
 	@PostMapping("/sign-up")
 	public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -36,6 +44,7 @@ public class UserController {
 		return "회원가입 성공";
 	}
 
+	@Hidden
 	@GetMapping("/jwt-test")
 	public String jwtTest() {
 		return "jwtTest 요청 성공";
