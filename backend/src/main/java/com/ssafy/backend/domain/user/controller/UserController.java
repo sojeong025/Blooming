d9fa13d0d143c6cc8fcdd3a92c0510dbc6dd6d6a
김성인 <kimsungin5@gmail.com>
2023-07-25 14:45:08 +0900

    comment: REFRESH_TOKEN DB 저장 기능 추가로 TODO 주석 제거

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -1,44 +1,43 @@
 package com.ssafy.backend.domain.user.controller;
 
-import com.ssafy.backend.domain.user.User;
-import com.ssafy.backend.domain.user.dto.UserSignUpDto;
-import com.ssafy.backend.domain.user.service.UserService;
-import com.ssafy.backend.global.jwt.service.JwtService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import com.ssafy.backend.domain.user.dto.UserSignUpDto;
+import com.ssafy.backend.domain.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
-    private final JwtService jwtService;
+	private final UserService userService;
+	// private final JwtService jwtService;
 
-    @GetMapping("/oauth2/sign-up")
-    public String signUpForm() {
-        return "sign-up";
-    }
+	@GetMapping("/oauth2/sign-up")
+	public String signUpForm() {
+		return "sign-up";
+	}
 
-    // TODO: REFRESH_TOKEN 저장해야함.
-    @PostMapping("/sign-up")
-    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	@PostMapping("/sign-up")
+	public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || authentication.getName() == null) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
-        }
+		if (authentication == null || authentication.getName() == null) {
+			throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+		}
 
-        userService.signUp(userSignUpDto, authentication.getName());
-        return "회원가입 성공";
-    }
+		userService.signUp(userSignUpDto, authentication.getName());
+		return "회원가입 성공";
+	}
 
-    @GetMapping("/jwt-test")
-    public String jwtTest() {
-        return "jwtTest 요청 성공";
-    }
+	@GetMapping("/jwt-test")
+	public String jwtTest() {
+		return "jwtTest 요청 성공";
+	}
 }
