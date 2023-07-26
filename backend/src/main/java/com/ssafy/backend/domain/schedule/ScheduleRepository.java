02eded82c0f4cb3dd14faac8ed1d6538f9045018
김승연 <love00826@naver.com>
2023-07-26 16:12:32 +0900

    feat: spring boot jpa로 scheduleDate 찾기 구현 완료 (테스트 아직)

--- backend/src/main/java/com/ssafy/backend/domain/schedule/ScheduleRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/schedule/ScheduleRepository.java
@@ -3,54 +3,11 @@ package com.ssafy.backend.domain.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findAllByScheduleDate(LocalDate scheduleDate);
 
 }
-
-
-
-
-
-
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Repository;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@Repository
-//@RequiredArgsConstructor
-//public class ScheduleRepository {
-//
-//    @PersistenceContext
-//    private EntityManager em;
-//
-//    public List<Schedule> findByDate(int dayDiff){
-//        //h2 쿼리..
-//        List<Schedule> result = em.createQuery("select s from Schedule s where FORMATDATETIME(TIMESTAMPADD(DAY, :diff, NOW()), 'yyyy-MM-dd') " +
-//                " = FORMATDATETIME(s.scheduleDateTime, 'yyyy-MM-dd')", Schedule.class)
-//                .setParameter("diff", dayDiff)
-//                .getResultList();
-//        System.out.println(result);
-//        return result;
-//    }
-//
-//    public List getDate(){
-//        //h2 쿼리..
-//        List<Schedule> result = em.createQuery("select s from Schedule s where CURRENT_DATE " +
-//                        " = FORMATDATETIME(s.scheduleDateTime, 'yyyy-MM-dd')", Schedule.class)
-//                .getResultList();
-//        System.out.println(result);
-//        return result;
-//    }
-//
-//}
