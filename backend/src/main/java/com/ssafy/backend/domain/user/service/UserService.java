c1d294fb5150202d1ee8ba3e16643ee9592a83f0
김성인 <kimsungin5@gmail.com>
2023-07-26 17:45:20 +0900

    feat: 커플코드 입력 시 해당 커플코드의 커플 연결 기능 추가

--- backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
@@ -34,12 +34,18 @@ public class UserService {
         findUser.update(userSignUpDto);
         findUser.authorizeUser();
 
-        String coupleCode = "123ABCabc";// TODO: 커플코드 생성 함수 만들기
-        Couple couple = Couple.createCouple(coupleCode);
+        if (userSignUpDto.getCoupleCode() == null) {
+            String coupleCode = "123ABCabc";// TODO: 커플코드 생성 함수 만들기
+            Couple couple = Couple.createCouple(coupleCode);
 
-        findUser.setCouple(couple);
+            findUser.setCouple(couple);
+            coupleRepository.save(couple);
+        } else {
+            Couple couple = coupleRepository.findByCoupleCode(userSignUpDto.getCoupleCode())
+                .orElseThrow(() -> new IllegalArgumentException("입력한 커플 코드에 맞는 커플이 없습니다."));
 
-        coupleRepository.save(couple);
+            findUser.setCouple(couple);
+        }
 
         //        User user = User.builder()
 //                .email(userSignUpDto.getEmail())
