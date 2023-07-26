816f84697aa6b47948c5590f10fd3d384ab18398
김승연 <love00826@naver.com>
2023-07-26 15:54:03 +0900

    progress: 매 초 알림. 토큰 문자열로 적용

--- backend/src/main/java/com/ssafy/backend/fcm/NotificationScheduler.java
+++ backend/src/main/java/com/ssafy/backend/fcm/NotificationScheduler.java
@@ -7,6 +7,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import com.ssafy.backend.domain.schedule.Schedule;
+import com.ssafy.backend.domain.schedule.ScheduleRepository;
 import com.ssafy.backend.domain.user.User;
 import com.ssafy.backend.domain.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +21,8 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
-
-import static com.ssafy.backend.fcm.RequestPushMessage.MORNING_DIET;
 
 @Slf4j
 @Service
@@ -36,6 +37,8 @@ public class NotificationScheduler {
     private FirebaseMessaging firebaseMessaging;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
 
 
     @PostConstruct
@@ -51,19 +54,56 @@ public class NotificationScheduler {
     }
 
     //시간에 맞게 푸시 알림을 스케줄링하는 코드
-    @Scheduled(cron = "* * 11 * * ?")
+    @Scheduled(cron = "* * * * * ?")
     public void pushMorningDietAlarm() throws FirebaseMessagingException{
-        log.info("11시 매 분 알림");
+        log.info("매 시 매 초 알림");
 
         //여기서 일정 DB를 읽고 일정이 한 달, 삼 주, 일주일, 하루 전, 당일이면 알림을 보냄.
         //나중에 지난 일정은 삭제? 해도 될듯
         //일정 repository에서 day를 매개변수로 넘겨주면서, 30일, 21일, 7일, 1일, 0일 을 인자로 해서 date 비교해서 해당되는거 가져옴. 알림 보내고 테이블에 추가.
 
-        //30일 후
+        /*
+         30일 후
+         현재 날짜 + 30일 해서 해당 날짜의 일정인 경우, 일정 테이블에서 (커플 아이디, 카테고리, 일정 이름, 내용) DTO을 가져옴
+         카테고리에 따라 처리.
+         일단 공통으로 치면: 신랑 아이디와 신부 아이디를 유저 테이블에서 가져와서 (join) 해당 id를 타겟으로 서로 다른 메시지를 보냄
+
+         일단은 한 사람에게 같은 메시지 보내기
+
+insert into schedule (schedule_id, category, couple_id, schedule_at, title, content)
+values (1, '공통', 1, NOW(), '알림제목', '알림내용');
+
+select * from schedule;
+
+방법1. 쿼리로 찾기
+//지금으로부터 30일 후의 일정 얻기
+SELECT *
+FROM schedule
+WHERE
+FORMATDATETIME(TIMESTAMPADD(DAY, 30, NOW()), 'yyyy-MM-dd')
+= FORMATDATETIME(schedule_at, 'yyyy-MM-dd');
+
+방법2. spring boot jpa 만들기(함수명으로)
+....흠..
+findByScheduleAt(30일 후)
+
+         findBy
+         */
+
+        //일단은 쿼리로 짜보자
+        List<Schedule> schedules = scheduleRepository.findByDate(0);
+        System.out.println(schedules);
+
+        List test = scheduleRepository.getDate();
+        System.out.println(test);
+
+        Long targetId = 1L;
+        String subject = "30일 후 일정이 있습니다";
+        String content = "일정 내용은 머시기";
 
 
         //토큰, 일정 이름(Title), 상세 내용(body)을 보냄
-        String result = sendNotificationByToken(new FCMNotificationRequestDto(1L, "제목", "내용")); // 첫 번째로 넣은 유저
+        String result = sendNotificationByToken(new FCMNotificationRequestDto(targetId, subject, content)); // 첫 번째로 넣은 유저
         log.info(result);
 //        pushAlarm(MORNING_DIET);
 
