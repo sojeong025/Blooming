2e86db1acfba8bdf92849b3b7b1cca3650157051
정훈석 <wjdgnstjr45@naver.com>
2023-07-27 15:36:40 +0900

    progress 날짜별로 일정추가기능

--- frontend/blooming/src/components/Schedule/Task.jsx
+++ frontend/blooming/src/components/Schedule/Task.jsx
@@ -4,13 +4,7 @@ function formatDateToString(date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Month starts from 0, so we add 1 and pad with '0'
   const day = String(date.getDate()).padStart(2, '0'); // Pad the day with '0' if needed
-
-  let hours = date.getHours();
-  const ampm = hours >= 12 ? '오후' : '오전';
-  hours %= 12;
-  const minutes = String(date.getMinutes()).padStart(2, '0'); // Pad the minutes with '0' if needed
-
-  return `${year}-${month}-${day} ${ampm} ${hours}시`;
+  return `${year}-${month}-${day}`;
 }
 
 function Task ({ date, body }) {
---
+++ frontend/blooming/src/recoil/UserStateAtom.jsx
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+// 웨딩 날짜 데이터
+export const UserState = atom({
+  key: "UserState",
+  default: null,
+});
