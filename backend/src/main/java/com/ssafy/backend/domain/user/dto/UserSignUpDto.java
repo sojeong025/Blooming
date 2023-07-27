bb08bb0499c47717bab64996eff62beca4153bb3
김성인 <kimsungin5@gmail.com>
2023-07-27 15:40:31 +0900

    feat: 결혼식 날짜 조회 api 추가

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/UserSignUpDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/UserSignUpDto.java
@@ -6,12 +6,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class UserSignUpDto {
-
-    private String email;
-    private String password;
-    private String name;
-    private String nickname;
-    private String phoneNumber;
-    private String gender;
-    private String coupleCode;
+	private String name;
+	private String nickname;
+	private String phoneNumber;
+	private String gender;
+	private String coupleCode;
 }
