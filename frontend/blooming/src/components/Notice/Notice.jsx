9396988fdba4948601eee14b6f513bf4e4e087f4
HY <9gmldud@naver.com>
2023-07-28 08:57:04 +0900

    progress noticelist

--- frontend/blooming/src/components/Notice/Notice.jsx
+++ frontend/blooming/src/components/Notice/Notice.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
+import classes from "./Notice.module.css";
 
 const SwipeableList = () => {
   const [listItems, setListItems] = useState([
-    { id: 1, text: 'Item 1', x: 0, showButtons: false },
-    { id: 2, text: 'Item 2', x: 0, showButtons: false },
-    { id: 3, text: 'Item 3', x: 0, showButtons: false },
+    { id: 1, text: "Item 1", x: 0, showButtons: false },
+    { id: 2, text: "Item 2", x: 0, showButtons: false },
+    { id: 3, text: "Item 3", x: 0, showButtons: false },
     // Add more list items as needed
   ]);
 
@@ -14,45 +15,49 @@ const SwipeableList = () => {
     // 터치 시작 시 초기 x 위치를 저장합니다.
     startX = e.touches[0].clientX;
     setListItems((prevItems) =>
-      prevItems.map((item, i) => (i === index ? { ...item, x: 0 } : item))
+      prevItems.map((item, i) => (i === index ? { ...item, x: 0 } : item)),
     );
+    // console.log(startX);
   };
 
   const handleTouchMove = (index, e) => {
     // 터치 이동 중인 항목의 x 위치를 업데이트합니다.
     const deltaX = e.touches[0].clientX - startX;
+
+    // console.log(deltaX, listItems);
+    // 오른쪽으로 안가게 하고 싶은데
     setListItems((prevItems) =>
       prevItems.map((item, i) =>
-        i === index ? { ...item, x: Math.min(deltaX, 100) } : item
-      )
+        i === index ? { ...item, x: Math.max(deltaX, -100) } : item,
+      ),
     );
   };
 
   const handleTouchEnd = (index) => {
     setListItems((prevItems) =>
       prevItems.map((item, i) =>
-        i === index ? { ...item, x: 0, showButtons: item.x <= -100 } : item
-      )
+        i === index ? { ...item, x: 0, showButtons: item.x <= -100 } : item,
+      ),
     );
   };
 
   const handleSwipeEnd = (index) => {
-    console.log(index)
+    console.log(index);
     // 스와이프 종료 시 해당 아이템을 삭제 처리합니다.
     const updatedListItems = listItems.filter((_, i) => i !== index);
     setListItems(updatedListItems);
   };
 
   return (
-    <div>
+    <div className={classes.noticeContainer}>
       {listItems.map((item, index) => (
-        <div key={item.id}>
+        <div key={item.id} className={classes.noticeWrapper}>
           <div
+            className={classes.listItem}
             style={{
               transform: `translateX(${item.x}px)`,
-              cursor: 'grab',
-              userSelect: 'none', // 드래그 중 텍스트 선택 방지
-              position: 'relative',
+              cursor: "grab",
+              userSelect: "none", // 드래그 중 텍스트 선택 방지
             }}
             onTouchStart={(e) => handleTouchStart(index, e)}
             onTouchMove={(e) => handleTouchMove(index, e)}
@@ -61,42 +66,29 @@ const SwipeableList = () => {
             {item.text}
           </div>
           <div
-              style={{
-                display: item.showButtons ? 'flex' : 'none',
-                alignItems: 'center',
-                top: 0,
-                right: 100,
-                width: 200,
-              }}
+            className={classes.actionButtons}
+            style={{
+              display: item.showButtons ? "flex" : "none",
+              // alignItems: "center",
+              // top: 0,
+              // right: 100,
+              // width: 200,
+            }}
+          >
+            <button
+              className={classes.deleteButton}
+              onClick={() => handleSwipeEnd(index)}
             >
-              <button
-                onClick={() => handleSwipeEnd(index)}
-                style={{
-                  width: 100,
-                  height: '100%',
-                  background: 'red',
-                  color: 'white',
-                  border: 'none',
-                  cursor: 'pointer',
-                }}
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => handleTouchEnd(index)}
-                style={{
-                  width: 100,
-                  height: '100%',
-                  background: 'gray',
-                  color: 'white',
-                  border: 'none',
-                  cursor: 'pointer',
-                }}
-              >
-                Cancel
-              </button>
-            </div>
+              삭제
+            </button>
+            <button
+              className={classes.cancelButton}
+              onClick={() => handleTouchEnd(index)}
+            >
+              취소
+            </button>
           </div>
+        </div>
       ))}
     </div>
   );
