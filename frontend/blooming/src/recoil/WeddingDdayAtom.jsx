41eb96553090aa91e056f8eefd4a03bec633e1f6
정소정 <thwjd0205@naver.com>
2023-07-28 11:50:17 +0900

    fix: wedding date

--- frontend/blooming/src/recoil/WeddingDdayAtom.jsx
+++ frontend/blooming/src/recoil/WeddingDdayAtom.jsx
@@ -3,7 +3,7 @@ import { atom } from "recoil";
 // 웨딩 날짜 데이터
 export const weddingDateState = atom({
   key: "weddingDateState",
-  default: "2024-01-25",
+  default: "2024-03-14",
 });
 
 export const weddingDdayState = atom({
