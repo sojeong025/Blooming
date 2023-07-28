2f78c9ce3533f8d9ea98f89ede156b316296b180
HY <9gmldud@naver.com>
2023-07-28 15:23:18 +0900

    progress Notice

--- frontend/blooming/src/Pages/Notice/AllNotice.jsx
+++ frontend/blooming/src/Pages/Notice/AllNotice.jsx
@@ -1,5 +1,5 @@
 import Notice from "../../components/Notice/Notice";
-// import NoticeLibrary from "../../components/Notice/Notice_library";
+import NoticeLibrary from "../../components/Notice/Notice_library";
 import { useNavigate } from "react-router-dom";
 
 export default function AllNotice() {
@@ -12,7 +12,7 @@ export default function AllNotice() {
     <>
       <div onClick={handleHistory}>엑스버튼</div>
       <Notice />
-      {/* <NoticeLibrary /> */}
+      <NoticeLibrary />
     </>
   );
 }
---
+++ frontend/blooming/src/components/Notice/Notice_library.jsx
@@ -0,0 +1,42 @@
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
+
+const Notice = () => {
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={() => console.info("swipe action triggered")}>
+        Action name
+      </SwipeAction>
+    </LeadingActions>
+  );
+
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction
+        destructive={true}
+        onClick={() => console.info("swipe action triggered")}
+      >
+        Delete
+      </SwipeAction>
+    </TrailingActions>
+  );
+
+  return (
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+        Item content
+      </SwipeableListItem>
+    </SwipeableList>
+  );
+};
+
+export default Notice;
