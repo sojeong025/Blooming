7245834624bba7c1a9be8abe59d3ec9f08d02513
구희영 <9hiyoung@gmail.com>
2023-07-30 20:23:53 +0900

    progress: TopAppBar 수정

--- frontend/blooming/src/components/Common/TopAppBar.jsx
+++ frontend/blooming/src/components/Common/TopAppBar.jsx
@@ -1,29 +1,56 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 import classes from "./TopAppBar.module.css";
 
 import noticeBase from "../../assets/Nav/notice_base.svg";
 import noticeActive from "../../assets/Nav/notice_active.svg";
+import { useEffect, useState } from "react";
 
 const TopAppBar = () => {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    if (location.state && location.state.pageTitle) {
+      setPageTitle(location.state.pageTitle);
+    } else {
+      setPageTitle("");
+    }
+  }, [location]);
+
+  // 뒤로가기 필요하면 여기 넣기
+  const backIcon = ["/AllNotice"];
+  const navigate = useNavigate();
+  const handleHistory = () => {
+    navigate(-1);
+  };
+
   return (
     <header className={classes.header}>
-      <img src='src/assets/Nav/word.png' alt='Logo' className={classes.logo} />
-
-      <NavLink to='/AllNotice' className={classes.rightLink}>
-        <div className={classes.navBlock}>
-          <img
-            className={classes.navIcon}
-            // activeClassName에 의해 이미지 소스 변경
-            src={
-              window.location.pathname === "/AllNotice"
-                ? noticeActive
-                : noticeBase
-            }
-            alt=''
-          />
-          <div className={classes.navTitle}>알림</div>
+      <div className={`${classes.navIcon} ${classes.navLeft}`}>
+        <div onClick={handleHistory}>
+          {backIcon.includes(location.pathname) && <img src='' alt='뒤' />}
         </div>
+      </div>
+
+      {/* 가운데 로고 또는 페이지타이틀 */}
+      <div className={classes.pageTitleContainer}>
+        {!pageTitle && <img src='src/assets/Nav/word.png' alt='Logo' />}
+        <p>{pageTitle}</p>
+      </div>
+
+      {/* 알림창으로 이동 */}
+      <NavLink to='/AllNotice' state={{ pageTitle: "알림" }}>
+        {/* 알림 아이콘 24x24에 맞춰 넣기 */}
+        <img
+          className={`${classes.navIcon} ${classes.navRight}`}
+          src={
+            window.location.pathname === "/AllNotice"
+              ? noticeActive
+              : noticeBase
+          }
+          alt='알림'
+        />
       </NavLink>
     </header>
   );
