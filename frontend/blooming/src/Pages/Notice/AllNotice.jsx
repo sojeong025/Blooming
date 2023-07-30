fb53bf0cd2427fa86e895e5f50b6e9c3edfccc33
구희영 <9hiyoung@gmail.com>
2023-07-31 00:44:45 +0900

    progress 안쓰는거 지움

--- frontend/blooming/src/Pages/Notice/AllNotice.jsx
+++ frontend/blooming/src/Pages/Notice/AllNotice.jsx
@@ -1,16 +1,6 @@
-import { useEffect, useState } from "react";
 import NoticeList from "../../components/Notice/NoticeSwipeable";
 
 export default function AllNotice() {
-  const [notices, setNotices] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users/1/todos")
-      .then((response) => response.json())
-      .then((json) => setNotices(json));
-  }, []);
-  // console.log(notices);
-
   return (
     <div style={{ padding: "60px 0" }}>
       <NoticeList />
