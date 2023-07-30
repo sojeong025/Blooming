9f033aa847804bf80c349758dec2507472730982
구희영 <9hiyoung@gmail.com>
2023-07-30 23:18:57 +0900

    style: mainContainer padding

--- frontend/blooming/src/components/Home/WeddingDday.jsx
+++ frontend/blooming/src/components/Home/WeddingDday.jsx
@@ -64,7 +64,7 @@ export const WeddingDday = () => {
   };
 
   return (
-    <div className='topContainer'>
+    <div>
       <input type='date' value={weddingDate} onChange={handleChange} readOnly />
       <p>{renderScript()}</p>
     </div>
