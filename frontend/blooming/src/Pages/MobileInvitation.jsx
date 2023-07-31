2c095a9455e92e3d7118ae8cba6dd53e745487a0
정소정 <thwjd0205@naver.com>
2023-07-31 09:17:54 +0900

    progress #1: make a mobile invitation

---
+++ frontend/blooming/src/Pages/MobileInvitation.jsx
@@ -0,0 +1,10 @@
+function MobileInvitation() {
+    return (
+        <div>
+        <h1>모바일 청첩장 페이지</h1>
+        {/* 청첩장 내용 및 디자인 요소를 추가 */}
+        </div>
+    );
+};
+
+export default MobileInvitation;
---
+++ frontend/blooming/src/components/Home/TipBox.jsx
@@ -0,0 +1,9 @@
+function Tipbox() {
+  return (
+    <div>
+      정소정 바보
+    </div>
+  )
+}
+
+export default Tipbox;
