884b4520cd12ae1964d20fac79bc2ebb8bcfc9dc
구희영 <9gmldud@naver.com>
2023-08-01 12:20:07 +0900

    progress: input 모듈 제작해보기

--- frontend/blooming/src/Pages/Error.jsx
+++ frontend/blooming/src/Pages/Error.jsx
@@ -1,5 +1,6 @@
 import ErrorModal from "../components/Error/ErrorModal";
 import useErrorModal from "../components/Error/useErrorModal";
+import InputField from "../components/MyPage/InputField";
 
 import Modal from "../components/Error/Modal";
 import { useState } from "react";
@@ -15,6 +16,17 @@ const Error = () => {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  // 모듈테스트
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    displayName: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div className='mainContainer'>
       <p>여긴 사공사</p>
@@ -28,6 +40,19 @@ const Error = () => {
         <h2>Error</h2>
         <p>이런저런에러</p>
       </Modal>
+
+      <div style={{ border: "1px solid black" }}>
+        <h2>모듈테스트다</h2>
+        <form>
+          <InputField
+            label='이메일'
+            type='email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
     </div>
   );
 };
