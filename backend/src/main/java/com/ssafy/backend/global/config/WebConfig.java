b0557d488477e66800aed936ce3203a7b4019d7c
김성인 <kimsungin5@gmail.com>
2023-08-02 14:12:08 +0900

    fix: access 토큰 cors 해결

--- backend/src/main/java/com/ssafy/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/backend/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedOrigins("http://43.200.254.50", "http://192.168.30.207:5173", "http://127.0.0.1:5173",
 				"http://localhost:5173") // 프론트 ip로 변경하기
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "FETCH", "HEAD", "OPTIONS")
-			.allowedHeaders("*")
+			.allowedHeaders("Authorization", "Authorization_refresh")
 			.exposedHeaders("Authorization", "Authorization_refresh")
 			.allowCredentials(true)
 			.maxAge(3600);
