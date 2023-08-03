d16fd8ca8809d7ba678319daaa575267c7e7be99
정훈석 <wjdgnstjr45@naver.com>
2023-08-03 12:00:10 +0900

    progress #9 KakaoLogin.jsx 수정

--- frontend/blooming/src/Pages/Login/KakaoLogin.jsx
+++ frontend/blooming/src/Pages/Login/KakaoLogin.jsx
@@ -5,47 +5,44 @@ import { useEffect } from "react";
 
 function KakaoLogin() {
   const [access, setAccess] = useRecoilState(accessTokenState)
-  const [refresh, setRefresh] = useRecoilState(refreshTokenState)
+  // const [refresh, setRefresh] = useRecoilState(refreshTokenState)
   const location = useLocation();
   const navigate = useNavigate();
   const searchParams = new URLSearchParams(location.search);
 
   // Access Token과 Refresh Token 추출
   const accessTokenParam = searchParams.get("access_token");
-  const refreshTokenParam = searchParams.get("refresh_token");
+  // const refreshTokenParam = searchParams.get("refresh_token");
   const isUser = searchParams.get("is_user");
 
   // Access Token과 Refresh Token에서 "Bearer " 부분을 제거하고, 공백을 제거
   const accessToken = accessTokenParam ? accessTokenParam.replace("Bearer ", "").trim() : null;
-  const refreshToken = refreshTokenParam ? refreshTokenParam.replace("Bearer ", "").trim() : null;
+  // const refreshToken = refreshTokenParam ? refreshTokenParam.replace("Bearer ", "").trim() : null;
 
   // Access Token과 Refresh Token 출력 (디버깅용)
   console.log("Access Token:", accessToken);
-  console.log("Refresh Token:", refreshToken);
+  // console.log("Refresh Token:", refreshToken);
 
   // accessToken이 있는 경우 /join 페이지로 이동
-  useEffect(() => {
-    if (refreshToken) {
-      setRefresh(refreshToken)
-      localStorage.setItem('refreshToken', refreshToken)
-    }
-    if (accessToken) {
-      setAccess(accessToken);
-      localStorage.setItem('accessToken', accessToken)
-    }
-  }, [accessToken]);
-
-  useEffect(() => {
-    // access
-    console.log(isUser)
-    if (access) {
-      if (isUser === 'T') {
-        navigate("/home")
-      } else if (isUser === 'F') {
-        navigate("/join");
+  const goTo = async () => {
+    if (refreshToken && accessToken) {
+      await localStorage.setItem('accessToken', accessToken)
+      setAccess(accessToken)
+      // await localStorage.setItem('refreshToken', refreshToken)
+      // setRefresh(refreshToken)
+      if (access) {
+        if (isUser === 'T') {
+          navigate("/home")
+        } else if (isUser === 'F') {
+          navigate("/join");
+        }
       }
     }
-  }, [access]);
+  }
+
+  useEffect(() => {
+    goTo()
+  }, []);
 }
 
 export default KakaoLogin;
