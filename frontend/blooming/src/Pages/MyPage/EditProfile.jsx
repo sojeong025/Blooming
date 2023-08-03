a7f4b9a9138b7b3cb0c0fd4838b62e73184f790c
구희영 <9gmldud@naver.com>
2023-08-04 04:01:35 +0900

    progress put profile

--- frontend/blooming/src/Pages/MyPage/EditProfile.jsx
+++ frontend/blooming/src/Pages/MyPage/EditProfile.jsx
@@ -1,46 +1,82 @@
 import { useRecoilState } from "recoil";
 import { userState } from "../../recoil/ProfileAtom";
 import classes from "./EditProfile.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { customAxios } from "../../lib/axios";
+import InputForm from "../../components/Common/InputText";
+import { useNavigate } from "react-router-dom";
 
 const EditProfile = () => {
   const [userData, setUserData] = useRecoilState(userState);
 
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    nickname: "",
+    phoneNumber: "",
+    gender: "",
+    coupleCode: 0,
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setFormData({ ...userData });
+  }, []);
+
   const updateUserData = async () => {
     try {
       const response = await customAxios.put(
-        "/profile",
+        "profile",
         // 수정할 데이터
-        {},
+        { ...formData },
       );
-      console.log(response);
+      setUserData({ ...formData });
+      navigate("/mypage");
+      // console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // 정보 입력
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const putSubmit = async (event) => {
+    event.preventDefault();
+    updateUserData();
+  };
+
   return (
     <div className={`mainContainer ${classes.EditContainer}`}>
-      {/* <div>
-        이거는 알림 버튼에 바꿀까 수정용 앱바를 만들어야겠어
-        뒤로가기-회원정보수정-완료버튼
-      </div> */}
       <div className={classes.profileImg}>
         <img src={userData.profileImg} alt='' />
       </div>
-      <div>이름</div>
-      <input type='text' />
-      <div>닉네임</div>
-      <input type='text' />
-      <label htmlFor=''>이메일</label>
-      <input type='text' />
-
-      <button>수정</button>
-
-      <form onSubmit={updateUserData}>
-        <label htmlFor=''>이름</label>
-        <button type='submit' onClick={updateUserData}>
+      <form onSubmit={putSubmit}>
+        <InputForm
+          label='이름'
+          name='name'
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <InputForm
+          label='닉네임'
+          name='nickname'
+          value={formData.nickname}
+          onChange={handleChange}
+        />
+        <InputForm
+          label='전화번호'
+          name='phoneNumber'
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          placeholder='전화번호를 작성해주세요.'
+          required
+        />
+        <button type='submit' className={classes.submitButton}>
           제출
         </button>
       </form>
