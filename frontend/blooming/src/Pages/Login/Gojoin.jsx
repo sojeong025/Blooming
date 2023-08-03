bfea6512b70dfb9cf09a4ab7168345cd4284b08b
정훈석 <wjdgnstjr45@naver.com>
2023-08-03 14:38:50 +0900

    feat: kakaodumy page

---
+++ frontend/blooming/src/Pages/Login/Gojoin.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import Button from '../../components/Login/Button'
+
+export default function Gojoin() {
+  
+  return (
+    <NavLink to={"/join"} className='mainContainer'>
+      <Button text="추가정보 입력하러 가기" />
+    </NavLink>
+  );
+}
