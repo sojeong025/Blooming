4daad1e37f799b5d2c5c5c7171ac1862ce2ce499
김성인 <kimsungin5@gmail.com>
2023-08-04 09:37:51 +0900

    refactor: invitation 관련 리팩터링

--- backend/src/main/java/com/ssafy/backend/domain/invitation/service/InvitationService.java
+++ backend/src/main/java/com/ssafy/backend/domain/invitation/service/InvitationService.java
@@ -1,85 +1,57 @@
 package com.ssafy.backend.domain.invitation.service;
 
-import com.ssafy.backend.domain.couple.Couple;
-import com.ssafy.backend.domain.invitation.Invitation;
-import com.ssafy.backend.domain.invitation.dto.InvitationRegistDto;
-import com.ssafy.backend.domain.invitation.dto.InvitationResultDto;
-import com.ssafy.backend.domain.invitation.exception.InvitationNotFoundException;
-import com.ssafy.backend.domain.invitation.repository.InvitationRepository;
-import com.ssafy.backend.domain.user.User;
-import com.ssafy.backend.domain.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
+
+import com.ssafy.backend.domain.couple.Couple;
+import com.ssafy.backend.domain.invitation.Invitation;
+import com.ssafy.backend.domain.invitation.dto.InvitationRegistDto;
+import com.ssafy.backend.domain.invitation.repository.InvitationRepository;
+import com.ssafy.backend.domain.user.User;
+import com.ssafy.backend.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class InvitationService {
-    private final InvitationRepository invitationRepository;
-    private final UserRepository userRepository;
+	private final InvitationRepository invitationRepository;
+	private final UserRepository userRepository;
 
-    public void registInvitation(InvitationRegistDto invitationRegistDto) {
-        //이게맞나 : dto -> entity 변환
-        Invitation invitation = new Invitation(
-                invitationRegistDto.getThumbnail(),
-                invitationRegistDto.getGroomFatherName(),
-                invitationRegistDto.getGroomFatherPhone(),
-                invitationRegistDto.getGroomMotherName(),
-                invitationRegistDto.getGroomMotherPhone(),
-                invitationRegistDto.getBrideFatherName(),
-                invitationRegistDto.getBrideFatherPhone(),
-                invitationRegistDto.getBrideMotherName(),
-                invitationRegistDto.getBrideMotherPhone(),
-                invitationRegistDto.getTitle(),
-                invitationRegistDto.getContent(),
-                invitationRegistDto.getWeddingHallName(),
-                invitationRegistDto.getFloor(),
-                invitationRegistDto.getAddress(),
-                invitationRegistDto.getDate(),
-                invitationRegistDto.getTime(),
-                invitationRegistDto.getGroomName(),
-                invitationRegistDto.getGroomPhone(),
-                invitationRegistDto.getBrideName(),
-                invitationRegistDto.getBridePhone()
-        );
-        
-        //유저 찾기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByEmail(authentication.getName())
-                .orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
-        Couple couple = user.getCouple();
+	public void registInvitation(InvitationRegistDto invitationRegistDto) {
+		// dto -> entity 변환
+		Invitation invitation = new Invitation(invitationRegistDto);
 
-        //커플 매핑
-        invitation.setCouple(couple);
+		//유저 찾기
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User user = userRepository.findByEmail(authentication.getName())
+				.orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
+		Couple couple = user.getCouple();
 
-        invitationRepository.save(invitation);
-    }
+		//커플 매핑
+		invitation.setCouple(couple);
 
-    public Invitation getInvitation() {
-        try {
-            //유저 찾기
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            User user = userRepository.findByEmail(authentication.getName())
-                .orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
-            Couple couple = user.getCouple();
+		invitationRepository.save(invitation);
+	}
 
-            //커플에 해당하는 청첩장 반환
-            return invitationRepository.findByCouple(couple)
-                .orElseThrow(() -> new InvitationNotFoundException("만든 청첩장이 없습니다."));
-        } catch (IllegalArgumentException ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
-        }
-    }
+	public Invitation getInvitation() {
+		//유저 찾기
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User user = userRepository.findByEmail(authentication.getName())
+				.orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
+		Couple couple = user.getCouple();
 
-    public void modifyInvitation(Long invitationId, InvitationRegistDto invitationRegistDto) {
-        Invitation invitation = invitationRepository.findById(invitationId)
-                .orElseThrow(() -> new IllegalArgumentException("invitationId에 해당하는 청첩장이 없습니다."));
-        invitation.update(invitationRegistDto);
-    }
+		//커플에 해당하는 청첩장 반환
+		return invitationRepository.findByCouple(couple)
+				.orElse(null);
+	}
+
+	public void modifyInvitation(Long invitationId, InvitationRegistDto invitationRegistDto) {
+		Invitation invitation = invitationRepository.findById(invitationId)
+				.orElseThrow(() -> new IllegalArgumentException("invitationId에 해당하는 청첩장이 없습니다."));
+		invitation.update(invitationRegistDto);
+	}
 }
