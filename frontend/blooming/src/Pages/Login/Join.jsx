966d5b774886704ee3f7bbd18d0c5cbc929863f2
정훈석 <wjdgnstjr45@naver.com>
2023-08-04 16:18:45 +0900

    progress #25 join

--- frontend/blooming/src/Pages/Login/Join.jsx
+++ frontend/blooming/src/Pages/Login/Join.jsx
@@ -49,7 +49,6 @@ export default function Join() {
   const getKakaoProfile = async () => {
     try {
       const response = await customAxios.get("kakao-profile");
-      // console.log(response.data.result[0]);
       const kakaoData = response.data.result[0];
       setFormData({ ...formData, ...kakaoData });
     } catch (error) {
@@ -87,7 +86,7 @@ export default function Join() {
   const handleSignUp = async () => {
     const currentFcmToken = await getToken();
     let customData = formData
-    console.log('여기가 중요', userData.coupleCode)
+    console.log('여기가 중요', userData, userData.coupleCode)
     if (userData.coupleCode) {
       customData = {
         ...formData,
