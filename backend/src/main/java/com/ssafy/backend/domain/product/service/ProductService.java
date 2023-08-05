71ba2efbce6ebc49c7046ee55c29561e06a0bec1
김승연 <love00826@naver.com>
2023-08-05 16:54:38 +0900

    fix: 상품 get 시 productType의 type 수정

--- backend/src/main/java/com/ssafy/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/ssafy/backend/domain/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.domain.product.service;
 
 import com.ssafy.backend.domain.product.Product;
+import com.ssafy.backend.domain.product.ProductType;
 import com.ssafy.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getTypeProduct(String productType) {
-        return productRepository.findByProductType(productType);
+    public List<Product> getTypeProduct(ProductType productType) {
+        return productRepository.findByProductType(String.valueOf(productType));
     }
 }
