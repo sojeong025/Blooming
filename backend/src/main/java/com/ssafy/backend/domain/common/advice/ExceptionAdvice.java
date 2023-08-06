daa925a5b19823fbdabf565633f1ecdf59fd1d70
김성인 <kimsungin5@gmail.com>
2023-08-07 02:24:44 +0900

    feat: s3에 사진 올리고 해당 url과 사진이름을 받는 api 추가

--- backend/src/main/java/com/ssafy/backend/domain/common/advice/ExceptionAdvice.java
+++ backend/src/main/java/com/ssafy/backend/domain/common/advice/ExceptionAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
 
@@ -25,6 +27,16 @@ public class ExceptionAdvice {
             .code(HttpStatus.BAD_REQUEST.value())
             .httpStatus(HttpStatus.BAD_REQUEST)
             .message(e.getMessage()).build();
+
+        return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = IOException.class)
+    public ResponseEntity<BasicResponse> ExceptionHandler(IOException e) {
+        BasicResponse basicResponse = BasicResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .message(e.getMessage()).build();
 
         return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
     }
