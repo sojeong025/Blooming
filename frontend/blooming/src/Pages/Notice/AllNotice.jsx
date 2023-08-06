e829c25ce884531147efd448e8ccbb3ef476bc1d
구희영 <9gmldud@naver.com>
2023-08-07 02:41:43 +0900

    progress pull To Refresh Notice

--- frontend/blooming/src/Pages/Notice/AllNotice.jsx
+++ frontend/blooming/src/Pages/Notice/AllNotice.jsx
@@ -1,9 +1,26 @@
 import NoticeList from "../../components/Notice/NoticeSwipeable";
+import { useState } from "react";
+import { PullToRefresh } from "antd-mobile";
 
 export default function AllNotice() {
+  const [data, setData] = useState(0);
+
+  const onReNotice = () => {
+    // 알림 정보 새로 고침
+    setData(data + 1);
+  };
   return (
     <div style={{ padding: "60px 0" }}>
-      <NoticeList />
+      <PullToRefresh
+        pullingText='당겨서 새로고침'
+        canReleaseText='당겨서 새로고침'
+        completeText='로딩중...'
+        refreshingText='로딩중...'
+        onRefresh={onReNotice}
+      >
+        {data}
+        <NoticeList />
+      </PullToRefresh>
     </div>
   );
 }
