2b661504e71c75e716e9d4bf9266c90cf55ad33c
정소정 <thwjd0205@naver.com>
2023-08-06 19:23:16 +0900

    style: schedule page but will use library

--- frontend/blooming/src/components/Schedule/ScheduleBody.jsx
+++ frontend/blooming/src/components/Schedule/ScheduleBody.jsx
@@ -1,12 +1,27 @@
+import { useRecoilValue } from 'recoil';
+import { ScheduleState } from '../../recoil/ScheduleStateAtom';
+import classes from './ScheduleBody.module.css'
+
 function ScheduleBody({ onCreatePost }) {
+  const selectedDate = useRecoilValue(ScheduleState);
+
+  const formatDate = (date) => {
+    const options = { weekday: 'long', year: 'numeric',
+                      month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
+
   return (
-    <header>
-      <p>
-        <button onClick={onCreatePost}>
-          +
-        </button>
-      </p>
-    </header>
+    <div className={classes.contatiner}>
+      <div className={classes.word}>
+        <p className={classes.select}>{formatDate(selectedDate)}</p>
+        <p className={classes.count}>오늘의 일정은 몇 개 입니다</p>
+      </div>
+      <div className={classes.btn}>
+        {/* 이미지로 수정해야함 */}
+        <button onClick={onCreatePost}> + </button>
+      </div>
+    </div>
   );
 }
 
