f10004792059004590139e75d8c9b13f95b53c19
정훈석 <wjdgnstjr45@naver.com>
2023-08-07 17:34:45 +0900

    progress #8 atom change

--- frontend/blooming/src/components/MyPage/MyWishlist.jsx
+++ frontend/blooming/src/components/MyPage/MyWishlist.jsx
@@ -1,16 +1,18 @@
 import { customAxios } from "../../lib/axios";
 import { useRecoilState } from "recoil";
-import { wishlistState } from "../../recoil/ProfileAtom";
+import { wishlistState, fianceWishlistState } from "../../recoil/ProfileAtom";
 import { useEffect } from "react";
 
 export default function MyWishlist() {
 
   const [myWishlist, setMyWishlist] = useRecoilState(wishlistState)
+  const [fianceWishlist, setFianceWishlist] = useRecoilState(fianceWishlistState)
 
   const fetchData = async () => {
     try {
       const response = await customAxios.get("wishlist");
-      setMyWishlist(response.data.result[0])
+      setMyWishlist(response.data.result[0].mywishlist)
+      setFianceWishlist(response.data.result[0].couplewishlist)
     } catch (error) {
       console.error("예약 정보 조회 에러:", error);
     }
@@ -23,6 +25,7 @@ export default function MyWishlist() {
   return (
     <div style={{marginTop: '56px'}}>
       {myWishlist}
+      {fianceWishlist}
     </div>
   );
 }
--- frontend/blooming/src/recoil/ScheduleStateAtom.jsx
+++ frontend/blooming/src/recoil/ScheduleStateAtom.jsx
@@ -4,3 +4,8 @@ export const ScheduleState = atom({
   key: 'ScheduleState',
   default: new Date(), // Date 객체를 default 값으로 사용
 });
+
+export const ScheduleTaskState = atom({
+  key: 'ScheduleTaskState',
+  default: [],
+});
