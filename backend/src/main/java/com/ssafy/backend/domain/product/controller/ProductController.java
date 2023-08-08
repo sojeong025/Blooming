93db75dc50b17bb7b85e8ddcc469f406bf58e585
김성인 <kimsungin5@gmail.com>
2023-08-08 12:03:35 +0900

    feat: 상세 페이지 진입 시 상품 사진을 주는 api 추가

--- backend/src/main/java/com/ssafy/backend/domain/product/controller/ProductController.java
+++ backend/src/main/java/com/ssafy/backend/domain/product/controller/ProductController.java
@@ -1,61 +1,81 @@
 package com.ssafy.backend.domain.product.controller;
 
-import com.ssafy.backend.domain.common.BasicResponse;
-import com.ssafy.backend.domain.product.ProductType;
-import com.ssafy.backend.domain.product.dto.ProductResultDto;
-import com.ssafy.backend.domain.product.service.ProductService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import com.ssafy.backend.domain.common.BasicResponse;
+import com.ssafy.backend.domain.product.ProductType;
+import com.ssafy.backend.domain.product.dto.ProductDetailDto;
+import com.ssafy.backend.domain.product.dto.ProductResultDto;
+import com.ssafy.backend.domain.product.service.ProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "상품 API", description = "상품 읽기 API")
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
-    //상품 읽기 : 타입별 읽기(타입), 하나만 읽기(상품 id.. 필요한가)
-    //지역 추가되면 : 지역별 읽기
+	//상품 읽기 : 타입별 읽기(타입), 하나만 읽기(상품 id.. 필요한가)
+	//지역 추가되면 : 지역별 읽기
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    @Operation(summary = "상품 타입별로 상품 한 페이지(4개씩) 조회하기", description = "지정된 타입의 상품을 한 페이지만큼 가져옵니다.")
-    @GetMapping("/product/{productType}")
-    @Parameter(name = "페이지 번호")
-    public ResponseEntity<BasicResponse> getTypeProduct(@PathVariable ProductType productType, @RequestParam int page, @RequestParam int size){
-        Slice<ProductResultDto> productDtoList = productService.getTypeProduct(productType, page, size);
+	@Operation(summary = "상품 타입별로 상품 한 페이지(4개씩) 조회하기", description = "지정된 타입의 상품을 한 페이지만큼 가져옵니다.")
+	@GetMapping("/product/{productType}")
+	public ResponseEntity<BasicResponse> getTypeProduct(@PathVariable ProductType productType,
+			@RequestParam @Parameter(name = "page", description = "요청하는 페이지") int page,
+			@RequestParam @Parameter(name = "size", description = "가져오려는 상품 개수") int size) {
 
-        BasicResponse basicResponse;
-        if (productDtoList.isEmpty()){
-            basicResponse = BasicResponse.builder()
-                    .code(HttpStatus.NO_CONTENT.value())
-                    .httpStatus(HttpStatus.NO_CONTENT)
-                    .message("조회하려는 상품이 없습니다.")
-                    .build();
-        }
-        else{
-            //dto로 변환
-//            List<ProductResultDto> productResultDtoList = products.stream()
-//                    .map(product -> new ProductResultDto(product.getId(), product.getItemName(), product.getBrief(), product.getThumbnail(), product.getDetailImage1(), product.getDetailImage2(), product.getDetailImage3(), product.getCompany(), product.getCompanyTime(), product.getCompanyAddress()))
-//                    .collect(Collectors.toList());
+		Slice<ProductResultDto> productDtoList = productService.getTypeProduct(productType, page, size);
 
-            basicResponse = BasicResponse.builder()
-                    .code(HttpStatus.OK.value())
-                    .httpStatus(HttpStatus.OK)
-                    .message("상품 타입에 대한 한 페이지 상품 조회 성공")
-                    .count(productDtoList.getNumberOfElements())
-                    .result(Collections.singletonList(productDtoList)).build();
-        }
-        return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
-    }
+		BasicResponse basicResponse;
+		if (productDtoList.isEmpty()) {
+			basicResponse = BasicResponse.builder()
+					.code(HttpStatus.NO_CONTENT.value())
+					.httpStatus(HttpStatus.NO_CONTENT)
+					.message("조회하려는 상품이 없습니다.")
+					.build();
+		} else {
+			//dto로 변환
+			//            List<ProductResultDto> productResultDtoList = products.stream()
+			//                    .map(product -> new ProductResultDto(product.getId(), product.getItemName(), product.getBrief(), product.getThumbnail(), product.getDetailImage1(), product.getDetailImage2(), product.getDetailImage3(), product.getCompany(), product.getCompanyTime(), product.getCompanyAddress()))
+			//                    .collect(Collectors.toList());
+
+			basicResponse = BasicResponse.builder()
+					.code(HttpStatus.OK.value())
+					.httpStatus(HttpStatus.OK)
+					.message("상품 타입에 대한 한 페이지 상품 조회 성공")
+					.count(productDtoList.getNumberOfElements())
+					.result(Collections.singletonList(productDtoList)).build();
+		}
+		return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+	}
+
+	@Operation(summary = "상품 상세 조회", description = "원하는 상품을 상세히 조회합니다.")
+	@GetMapping("/product/{productId}")
+	public ResponseEntity<BasicResponse> getTypeProduct(@PathVariable long productId) {
+		List<ProductDetailDto> productDetail = productService.getProductImage(productId);
+
+		BasicResponse basicResponse = BasicResponse.builder()
+				.code(HttpStatus.OK.value())
+				.httpStatus(HttpStatus.OK)
+				.message("상품 타입에 대한 한 페이지 상품 조회 성공")
+				.count(productDetail.size())
+				.result(Collections.singletonList(productDetail)).build();
+
+		return new ResponseEntity<>(basicResponse, basicResponse.getHttpStatus());
+	}
 
 }
--- backend/src/main/java/com/ssafy/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/ssafy/backend/domain/product/service/ProductService.java
@@ -1,58 +1,50 @@
 package com.ssafy.backend.domain.product.service;
 
-import com.ssafy.backend.domain.product.ProductType;
-import com.ssafy.backend.domain.product.dto.ProductResultDto;
-import com.ssafy.backend.domain.product.repository.ProductRepository;
-import com.ssafy.backend.domain.user.User;
-import com.ssafy.backend.domain.user.repository.UserRepository;
-import com.ssafy.backend.domain.wishlist.repository.WishlistRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.backend.domain.product.Product;
+import com.ssafy.backend.domain.product.ProductType;
+import com.ssafy.backend.domain.product.dto.ProductDetailDto;
+import com.ssafy.backend.domain.product.dto.ProductResultDto;
+import com.ssafy.backend.domain.product.repository.ProductRepository;
+import com.ssafy.backend.domain.user.User;
+import com.ssafy.backend.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ProductService {
 
-    private final ProductRepository productRepository;
-    private final UserRepository userRepository;
+	private final ProductRepository productRepository;
+	private final UserRepository userRepository;
 
-    public Slice<ProductResultDto> getTypeProduct(ProductType productType, int page, int size) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByEmail(authentication.getName())
-                .orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
+	public Slice<ProductResultDto> getTypeProduct(ProductType productType, int page, int size) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User user = userRepository.findByEmail(authentication.getName())
+				.orElseThrow(() -> new IllegalArgumentException("JWT token: 회원 이메일에 해당하는 회원이 없습니다."));
 
-//        List<ProductResultDto> result = productRepository.getProductWithWish(user.getId(), productType, pageable);
-        PageRequest pageRequest = PageRequest.of(page, size);
+		PageRequest pageRequest = PageRequest.of(page, size);
 
-        //        //타입에 맞는 상품 받아옴
-//        List<Product> products = productRepository.findByProductType(productType, pageable);
-//
-//        //유저의 찜 받아옴
-//        List<Wishlist> wishlists = wishlistRepository.findAllByUserId(user.getId());
+		return productRepository.getProductWithWish(user, productType, pageRequest);
+	}
 
-        //필터링..
-//        List<ProductResultDto> productResultDtoList = new ArrayList<>();
-//        for (Product product : page.getContent()){
-//            ProductResultDto productResultDto = new ProductResultDto(
-//                    product.getId(),
-//                    product.getItemName(),
-//                    product.getBrief(),
-//                    product.getThumbnail(),
-//                    product.getCompany(),
-//                    product.getCompanyTime(),
-//                    product.getCompanyAddress(),
-//                     //수정
-//            );
-//        }
+	public List<ProductDetailDto> getProductImage(long productId) {
+		Product product = productRepository.findById(productId)
+				.orElseThrow(() -> new NoSuchElementException("해당하는 상품 정보가 없습니다."));
 
-        return productRepository.getProductWithWish(user, productType, pageRequest);
-    }
+		return product.getProductImages().stream()
+				.map(ProductDetailDto::new)
+				.collect(Collectors.toList());
+	}
 }
