5b28ed3e33a05a86948c4e261c620ffc2c5dcfc2
정훈석 <wjdgnstjr45@naver.com>
2023-08-09 11:48:09 +0900

    progress #12 infodetail Schedule NewTask fix

--- frontend/blooming/src/Pages/Schedule.jsx
+++ frontend/blooming/src/Pages/Schedule.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CalendarComponent from "../components/Schedule/CalendarComponent"
 import TasksList from "../components/Schedule/TasksList"
 import ScheduleBody from "../components/Schedule/ScheduleBody"
 import classes from './Schedule.module.css'
-
+import { useRecoilState } from "recoil";
+import { ScheduleTaskState } from "../recoil/ScheduleStateAtom";
+import { customAxios } from "../lib/axios";
 
 function Schedule() {
+  const [task, setTask] = useRecoilState(ScheduleTaskState)
   const [ modalIsVisible, setModalIsVisible ] = useState(false);
 
   function hideModalHandler() {
@@ -15,6 +18,20 @@ function Schedule() {
   function showModalHandler() {
     setModalIsVisible(true);
   }
+
+  async function fetchTask() {
+    try {
+      const response = await customAxios.get('scehdule')
+      console.log(response.data.result[0])
+      setTask(response.data.result[0])
+    } catch (error) {
+      console.log("스케쥴 받기 API", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTask()
+  }, [])
 
   return (
     <div className={classes.mainContainer}>
--- frontend/blooming/src/components/Schedule/NewTask.jsx
+++ frontend/blooming/src/components/Schedule/NewTask.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { ScheduleState } from '../../recoil/ScheduleStateAtom';
+import { userState } from '../../recoil/ProfileAtom';
 import DatePicker from 'react-datepicker'
 import './DatePicker.css'
 import classes from './NewTask.module.css';
+import { customAxios } from '../../lib/axios';
 
-function NewTask({onCancel, onAddTask, selectedDate}) {
+function NewTask({ onCancel, onAddTask, selectedDate }) {
+  const [ enteredTitle, setEnteredTitle ] = useState('');
   const [ enteredBody, setEnteredBody ] = useState('');
   const [ enteredDate, setEnteredDate ] = useRecoilState(ScheduleState);
+  const [ enteredTime, setEnteredTime ] = useState(new Date());
+  const user = useRecoilValue(userState)
+
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,13 +26,29 @@ function NewTask({onCancel, onAddTask, selectedDate}) {
     setEnteredDate(date);
   }
 
-  function submitHandler(event) {
+  function timeChangeHandler(date) {
+    setEnteredTime(date);
+  }
+
+  async function submitHandler(event) {
     event.preventDefault();
+    const formattedDate = `${enteredDate.getFullYear()}-${(enteredDate.getMonth() + 1).toString().padStart(2, '0')}-${enteredDate.getDate().toString().padStart(2, '0')}`;
+    const formattedTime = `${enteredTime.getHours().toString().padStart(2, '0')}:${enteredTime.getMinutes().toString().padStart(2, '0')}`;
+
     const taskData = {
-      body: enteredBody,
-      date: enteredDate,
+      title: enteredTitle,
+      content: enteredBody,
+      schelduleDate: formattedDate,
+      schelduleTime: formattedTime,
+      scehduledNy: user.gender,
+      scheduleType: "PRI",
     };
-    onAddTask(taskData);
+    try {
+      await customAxios.post('schedule', taskData)
+      onAddTask(taskData);
+    } catch (error) {
+      console.log('스케쥴 등록 API 에러', error)
+    }
     onCancel();
   }
 
@@ -39,14 +64,25 @@ function NewTask({onCancel, onAddTask, selectedDate}) {
           dateFormat="yyyy-MM-dd"
           required
         />
+        <label htmlFor="time">시간 선택</label>
+        <DatePicker
+          showPopperArrow={false}
+          id="time"
+          selected={enteredTime}
+          onChange={timeChangeHandler}
+          dateFormat="HH:mm"
+          required
+        />
       </div>
       <div>
-        <label htmlFor="body">일정이 뭐냐</label>
-        <textarea id="body" required rows={3} onChange={bodyChangeHandler} placeholder='일정을 입력하세요.' />
+        <label htmlFor="title">일정 제목</label>
+        <textarea id="title" required rows={1} onChange={titleChangeHandler} placeholder='제목을 입력하세요.' />
+        <label htmlFor="body">일정 내용</label>
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} placeholder='내용을 입력하세요.' />
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={onCancel}>취소</button>
-        <button>추가</button>
+        <button type='submit' >추가</button>
       </div>
     </form>
   );
