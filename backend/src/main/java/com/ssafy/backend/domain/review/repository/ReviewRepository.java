07506650d21889200a905efcee463ae828add29c
김성인 <kimsungin5@gmail.com>
2023-08-10 19:23:07 +0900

    fix: 상품 찾을 때 userEmail이 아닌 user 객체 넘기기

--- backend/src/main/java/com/ssafy/backend/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/review/repository/ReviewRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("select new com.ssafy.backend.domain.review.dto.ReviewResultDto(r, CASE WHEN COUNT(l) > 0 THEN true ELSE false END) from Review r left join fetch Liked l on l.review = r and l.user = :user where r.product.id = :productId")
+    @Query("select new com.ssafy.backend.domain.review.dto.ReviewResultDto(r, CASE WHEN COUNT(l) > 0 THEN true ELSE false END) from Review r left join fetch Liked l on l.review = r and l.user = :user where r.product.id = :productId group by r")
     Slice<ReviewResultDto> findReviewByProduct(@Param("productId") Long productId, @Param("user") User user, Pageable pageable);
 
     Slice<Review> findReviewByUser(User user, Pageable pageable);
