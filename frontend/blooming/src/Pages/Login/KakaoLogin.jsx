a09ab1cbeb4802126ac6dc86187ed97aeb408e72
Andrea <wjdgnstjr45@gmail.com>
2023-08-10 10:41:33 +0900

    fix: KakaoLogin

--- frontend/blooming/src/Pages/Login/KakaoLogin.jsx
+++ frontend/blooming/src/Pages/Login/KakaoLogin.jsx
@@ -1,36 +1,28 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function KakaoLogin() {
-  const [access, setAccess] = useState("");
-  // const [refresh, setRefresh] = useRecoilState(refreshTokenState)
   const location = useLocation();
   const navigate = useNavigate();
   const searchParams = new URLSearchParams(location.search);
 
-  // Access Token과 Refresh Token 추출
+  // Access Token 추출
   const accessTokenParam = searchParams.get("access_token");
-  // const refreshTokenParam = searchParams.get("refresh_token");
   const isUser = searchParams.get("is_user");
 
-  // Access Token과 Refresh Token에서 "Bearer " 부분을 제거하고, 공백을 제거
+  // Access Token에서 "Bearer " 부분을 제거하고, 공백을 제거
   const accessToken = accessTokenParam
     ? accessTokenParam.replace("Bearer ", "").trim()
     : null;
-  // const refreshToken = refreshTokenParam ? refreshTokenParam.replace("Bearer ", "").trim() : null;
 
-  // Access Token과 Refresh Token 출력 (디버깅용)
+  // Access Token 출력 (디버깅용)
   console.log("Access Token:", accessToken);
-  // console.log("Refresh Token:", refreshToken);
 
   // accessToken이 있는 경우 /join 페이지로 이동
-  const goTo = async () => {
+  const goTo = () => {
     if (accessToken) {
-      await localStorage.setItem("accessToken", accessToken);
-      await setAccess(accessToken);
-      // await localStorage.setItem('refreshToken', refreshToken)
-      // setRefresh(refreshToken)
-      if (access) {
+      localStorage.setItem("accessToken", accessToken);
+      if (localStorage.getItem("accessToken") === accessToken) {
         if (isUser === "T") {
           navigate("/home");
         } else if (isUser === "F") {
@@ -42,7 +34,7 @@ function KakaoLogin() {
 
   useEffect(() => {
     goTo();
-  }, [navigate, access]);
+  }, [navigate, accessToken]);
 }
 
 export default KakaoLogin;
