c737bbee709f5ea955cca6142f72a91dd1164ac4
구희영 <9gmldud@naver.com>
2023-08-10 12:11:21 +0900

    progress: info

--- frontend/blooming/src/components/Info/ProductItem.jsx
+++ frontend/blooming/src/components/Info/ProductItem.jsx
@@ -1,9 +1,61 @@
-export default function ProductItem({product, onClick}) {
+import { useState } from "react";
+import classes from "./ProductItem.module.css";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { customAxios } from "../../lib/axios";
+
+export default function ProductItem({ product, onClick }) {
+  const address = product.companyAddress.split(" ");
+  const [productHeart, setProductHeart] = useState(product.wish);
+
+  const handleCreateWish = async () => {
+    try {
+      await customAxios.post(`wishlist/${product.id}`);
+      setProductHeart(!productHeart);
+    } catch (error) {
+      console.error("찜하기 에러:", error);
+    }
+  };
+
+  const handleDeleteWish = async () => {
+    try {
+      await customAxios.delete(`wishlist/${product.id}`);
+      setProductHeart(!productHeart);
+    } catch (error) {
+      console.error("찜취소 에러:", error);
+    }
+  };
+
+  const onWish = () => {
+    // 찜
+    if (productHeart) {
+      // true이면 DELETE
+      handleCreateWish();
+    } else {
+      // false이면 POST
+      handleDeleteWish();
+    }
+  };
+
   return (
-    <div onClick={onClick}>
-      <img style={{width:'200px', height:'250px'}} src={product.thumbnail} alt="이미지 없음" />
-      <p>{product.company}</p>
-      <p>{product.itemName}</p>
+    <div
+      className={classes.ItemContainer}
+      // onClick={onClick}
+    >
+      <img
+        className={classes.itemImg}
+        src={product.thumbnail}
+        alt='이미지 없음'
+      />
+      <div className={classes.textContainer}>
+        <div className={classes.address}>
+          {address[0]} {address[1]}
+        </div>
+        <div className={classes.company}>{product.company}</div>
+        <div>{product.wish.toString()}</div>
+        <div className={classes.heart} onClick={onWish}>
+          {productHeart ? <AiFillHeart /> : <AiOutlineHeart />}
+        </div>
+      </div>
     </div>
   );
 }
