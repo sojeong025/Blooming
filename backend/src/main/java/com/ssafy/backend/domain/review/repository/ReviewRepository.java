ca12fe8d0d257fe1b332b3ce053c071c32e33323
김성인 <kimsungin5@gmail.com>
2023-08-11 01:49:43 +0900

    progress #4: 탈퇴 시 도움돼요, 후기 지우기

--- backend/src/main/java/com/ssafy/backend/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/review/repository/ReviewRepository.java
@@ -21,4 +21,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 //    @Query("select avg(r.star) as starRate, count(r) as reviewCount from Review r where r.product.id = :productId")
 //    Map<String, Object> findStarRate(@Param("productId") Long productId);
 
+    void deleteAllByUserId(Long userId);
+
 }
