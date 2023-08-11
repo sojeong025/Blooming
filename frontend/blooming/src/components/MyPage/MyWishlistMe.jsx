7e543a2e13e6243d4950e1f5bf70ab65f3511c11
정훈석 <wjdgnstjr45@naver.com>
2023-08-11 11:37:06 +0900

    progress #7 update MyWishlist.jsx InfoDetail.jsx

--- frontend/blooming/src/components/MyPage/MyWishlistMe.jsx
+++ frontend/blooming/src/components/MyPage/MyWishlistMe.jsx
@@ -1,32 +1,33 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-export default function MyWishlistMe({ wishlist }) {
-  
-  const navigate = useNavigate()
+function MyWishlistMe({ wishlist }) {
+  const navigate = useNavigate();
 
   const handleNavigation = (wish) => {
     navigate(`/info/${wish.productId}`, {
       state: { id: wish.productId, productType: wish.productType },
     });
   };
-  
+
   return (
     <>
-      {wishlist ?
-        <>
-          {wishlist.map((wish) => {
-            return (
-              <div key={wish.productId} onClick={() => handleNavigation(wish)}>
-                <img src={wish.thumbnail} alt="" />
-                <p>{wish.productType}</p>
-                <p>{wish.company}</p>
-                <p>{wish.address}</p>
-              </div>
-            )
-          })}
-        </>
-        :
-        <div>찜 목록이 없습니다.</div>}
+      {wishlist ? (
+        wishlist.map((wish) => (
+          <div
+            key={wish.productId}
+            onClick={() => handleNavigation(wish)}
+          >
+            <img src={wish.thumbnail} alt="" />
+            <p>{wish.productType}</p>
+            <p>{wish.company}</p>
+            <p>{wish.address}</p>
+          </div>
+        ))
+      ) : (
+        <div>찜 목록이 없습니다.</div>
+      )}
     </>
-  )
+  );
 }
+
+export default MyWishlistMe;
