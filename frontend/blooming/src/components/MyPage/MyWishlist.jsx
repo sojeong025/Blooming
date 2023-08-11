90a5773ecc2ae9d307eebe7e410ad5a748105d7c
정훈석 <wjdgnstjr45@naver.com>
2023-08-11 10:15:48 +0900

    progress #1 MyWishlistMe.jsx

--- frontend/blooming/src/components/MyPage/MyWishlist.jsx
+++ frontend/blooming/src/components/MyPage/MyWishlist.jsx
@@ -2,15 +2,45 @@ import { customAxios } from "../../lib/axios";
 import { useEffect, useState } from "react";
 
 import classes from "./MyWishlist.module.css";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../recoil/ProfileAtom"
+
+import MyWishlistMe from "./MyWishlistMe";
 
 export default function MyWishlist() {
 
+  const user = useRecoilValue(userState)
   const [state, setState] = useState('me')
+  const [myWishlist, setMyWishlist] = useState()
+  const [me, setMe] = useState()
+  const [you, setYou] = useState()
+  const [together, setTogether] = useState()
+
+  const classify = () => {
+    {myWishlist.map((wishlist) => {
+      if (wishlist.username === user.name) {
+        setMe([...me, wishlist])
+        if (you.some((item) => item.productId === wishlist.productId)) {
+          setTogether([...together, wishlist])
+        }
+      } else {
+        setYou([...you, wishlist])
+        if (me.some((item) => item.productId === wishlist.productId)) {
+          setTogether([...together, wishlist])
+        }
+      }
+    })}
+  }
 
   const fetchData = async () => {
     try {
       const response = await customAxios.get("wishlist");
-      console.log(response.data.result[0])
+      if (response.status === 204) {
+        return <div>찜한 정보가 없습니다.</div>
+      } else {
+        setMyWishlist(response.data.result[0])
+        classify()
+      }
     } catch (error) {
       console.error("예약 정보 조회 에러:", error);
     }
@@ -39,7 +69,7 @@ export default function MyWishlist() {
         <div onClick={handlerYouState}>약혼자 찜</div>
         <div onClick={handlerToState}>겹치는 찜</div>
       </nav>
-      {(state === 'me') && <div>내꺼</div>}
+      {(state === 'me') && <MyWishlistMe wishlist={me} /> }
       {(state === 'you') && <div>너꺼</div>}
       {(state === 'together') && <div>같아</div>}
     </div>
