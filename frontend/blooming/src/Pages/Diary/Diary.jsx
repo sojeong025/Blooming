de28fdf20b0ab6b9139320d508af90bd524797b1
정소정 <thwjd0205@naver.com>
2023-08-11 21:13:09 +0900

    progress #5: preview, detail 수정

--- frontend/blooming/src/Pages/Diary/Diary.jsx
+++ frontend/blooming/src/Pages/Diary/Diary.jsx
@@ -9,39 +9,9 @@ import DiaryHeader from "../../components/Diary/DiaryHeader";
 import { PiPencilLineFill } from "react-icons/pi"
 
 const Diary = () => {
-  // const [diaries, setDiaries] = useRecoilState(diaryState);
+  const [diaries, setDiaries] = useRecoilState(diaryState);
   const [loading, setLoading] = useState(true);
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const diaries = [
-    {
-      id: 1,
-      title: "Diary Entry 1",
-      content: "오늘은 친구들과 멋진 하루를 보냈습니다...주절주절 주절주절주절주절 주절주절주절주절 주절주절주절주절주절주절주절주절주절주절주절주절 주절주절주절주절주절주절 주절주절주절주절 주절주절주절주절",
-      date: "2023-08-11",
-      image: "/src/assets/test.jpg"
-    },
-    {
-      id: 2,
-      title: "Diary Entry 2",
-      content: "펫샵에 갔던 경험",
-      date: "2023-08-12",
-      image: "/src/assets/test2.jpg"
-    },
-    {
-      id: 3,
-      title: "Diary Entry 3",
-      content: "주말에 다녀온 휴양지 이야기",
-      date: "2023-08-18",
-      image: "/src/assets/diary.jpg"
-    },
-    {
-      id: 4,
-      title: "Diary Entry 4",
-      content: "독서 후 기록한 감상",
-      date: "2023-08-25",
-      image:"/src/assets/test3.jpg"
-    }
-  ];
 
   useEffect(() => {
     const fetchData = async () => {
