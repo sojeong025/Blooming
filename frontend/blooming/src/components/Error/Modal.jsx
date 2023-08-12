dd3cae88f8b0aecacaded8d6b04ef628a889985d
구희영 <9gmldud@naver.com>
2023-08-12 20:13:22 +0900

    progress: info detail review

--- frontend/blooming/src/components/Error/Modal.jsx
+++ frontend/blooming/src/components/Error/Modal.jsx
@@ -1,18 +1,54 @@
 // 모달
-import classes from "./Modal.module.css";
+import { useEffect } from "react";
+import { styled } from "styled-components";
 
-function Modal({ show, children, buttonText, onClose }) {
+function Modal({ show, children, onClose }) {
+  // 모달 보여졌을 때 스크롤 방지
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (show) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
+  }, [show]);
+
   return (
     show && (
       <>
-        <div className={classes.back}></div>
-        <div className={classes.modal}>
-          <div>{children}</div>
-          <button onClick={onClose}>{buttonText}</button>
-        </div>
+        <ModalOverlay onClick={onClose}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            {children}
+            {/* <button onClick={onClose}>닫기</button> */}
+          </ModalContent>
+        </ModalOverlay>
       </>
     )
   );
 }
-
 export default Modal;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 900;
+  /* transform: translateY(100%); */
+`;
+
+const ModalContent = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  /* 전체크기 */
+  /* min-width: 100vw;
+  min-height: 100vh; */
+  /* min-height: calc(100vh - 60px); */
+  padding: 20px;
+  z-index: 900;
+`;
--- frontend/blooming/src/components/Info/ProductDetailItem.jsx
+++ frontend/blooming/src/components/Info/ProductDetailItem.jsx
@@ -1,42 +1,28 @@
-import { useState } from "react";
-
 import classes from "./ProductDetailItem.module.css";
-import { MdLocationOn } from "react-icons/md";
 
 const ProductDetailItem = ({ product, productType }) => {
-  const address = product.companyAddress.split(" ");
-
   return (
     <>
-      <div className={classes.ProductTitle}>
-        <div className={classes.companyTitle}>{product.company}</div>
-        <div className={classes.location}>
-          <span className={classes.locationIcon}>
-            <MdLocationOn size={18} />
-          </span>
-          <span className={classes.address}>
-            {address[0]} {address[1]}
-          </span>
-        </div>
-      </div>
-      <hr className={classes.hr} />
-
       <div className={classes.detailContainer}>
         <img src={product.thumbnail} className={classes.companyImg}></img>
-        <div>
+        <div className={classes.titleBrief}>
           <div className={classes.subTitle}>{product.company}</div>
           <div className={classes.brief}>{product.brief}</div>
         </div>
       </div>
 
-      <div>주소 {product.companyAddress}</div>
-      <div>{product.companyTime !== "None" ? product.companyTime : "-"}</div>
-
-      {/* hall 뺴고 다 있음 */}
-      {/* 걍 뺄까 */}
-      {/* {productType !== "HALL" && (
-        <div className={classes.itemName}>{product.itemName}</div>
-      )} */}
+      <div className={classes.infoContainer}>
+        <div className={classes.item}>
+          <span className={classes.leftText}>주소</span>
+          <span className={classes.rightText}>{product.companyAddress}</span>
+        </div>
+        <div className={classes.item}>
+          <span className={classes.leftText}>영업 시간</span>
+          <span className={classes.rightText}>
+            {product.companyTime !== "None" ? product.companyTime : "-"}
+          </span>
+        </div>
+      </div>
     </>
   );
 };
