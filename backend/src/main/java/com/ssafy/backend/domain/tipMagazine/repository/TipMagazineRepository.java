85a8688dcdbe6b0860172befe0702f9f18068236
김승연 <love00826@naver.com>
2023-08-14 15:46:35 +0900

    feat: 메인 페이지 팁 매거진 API 생성

---
+++ backend/src/main/java/com/ssafy/backend/domain/tipMagazine/repository/TipMagazineRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.backend.domain.tipMagazine.repository;
+
+import com.ssafy.backend.domain.tipMagazine.TipMagazine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipMagazineRepository extends JpaRepository<TipMagazine, Long> {
+}
