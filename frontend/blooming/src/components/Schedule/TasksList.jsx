0c5b5fbc38b34bacd49a8f73d767f88a197b5771
정소정 <thwjd0205@naver.com>
2023-08-15 18:10:57 +0900

    style: 일정 개별적 완료 신랑/신부/공통 구분 + var(--color-point) 필요한 부분 수정

--- frontend/blooming/src/components/Schedule/TasksList.jsx
+++ frontend/blooming/src/components/Schedule/TasksList.jsx
@@ -4,9 +4,14 @@ import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { ScheduleState, ScheduleTaskState, TodayTaskState } from '../../recoil/ScheduleStateAtom';
 import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react';
+import femaleIcon from '/src/assets/Icon/female.png'
+import maleIcon from '/src/assets/Icon/male.png'
+import commonIcon from '/src/assets/Icon/common.png'
+
 
 function TasksList() {
   const tasks = useRecoilValue(ScheduleTaskState);
+
   const selectedDate = useRecoilValue(ScheduleState)
   const setTodayTask = useSetRecoilState(TodayTaskState)
 
@@ -32,10 +37,24 @@ function TasksList() {
       {tasks.length > 0 && (
         <ul className={ classes.posts } >
           {tasks.map((task) => {
+            let iconSrc;
+
+            switch (task.scheduledBy) {
+              case 'FEMALE':
+                iconSrc = femaleIcon;
+                break;
+              case 'MALE':
+                iconSrc = maleIcon;
+                break;
+              default:
+                iconSrc = commonIcon;
+            }
+
+
             if (formatDate(selectedDate) === formatDate(task.scheduleDate)) {
               return (
                 <NavLink to={`/schedule/${task.id}`} key={task.id}>
-                  <Task task={task} />
+                  <Task task={task}  iconSrc={iconSrc} scheduledBy={task.scheduledBy}/>
                 </NavLink>
               )
             }
