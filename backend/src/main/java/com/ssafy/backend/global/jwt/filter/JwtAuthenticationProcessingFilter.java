16c38d7377f2cf5d5417fb6253d7b9438b5dfd0f
김성인 <kimsungin5@gmail.com>
2023-08-15 21:28:04 +0900

    progress #1: refresh 토큰 값 확인을 위한 log 추가

--- backend/src/main/java/com/ssafy/backend/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ backend/src/main/java/com/ssafy/backend/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -46,6 +46,9 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
                 .filter(jwtService::isTokenValid)
                 .orElse(null);
 
+        log.debug(String.valueOf(request));
+        log.debug(refreshToken);
+
         // 리프레시 토큰이 요청 헤더에 존재했다면, 사용자가 AccessToken이 만료되어서
         // RefreshToken까지 보낸 것이므로 리프레시 토큰이 DB의 리프레시 토큰과 일치하는지 판단 후,
         // 일치한다면 AccessToken을 재발급해준다.
