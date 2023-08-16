6e4b4e0c955bda0ee9f774b1661c69eec2b5cd6a
구희영 <9gmldud@naver.com>
2023-08-16 14:42:02 +0900

    fix

--- frontend/blooming/src/Pages/Info/Info.jsx
+++ frontend/blooming/src/Pages/Info/Info.jsx
@@ -1,41 +1,85 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 
 import classes from "./Info.module.css";
+import { useState } from "react";
 
 function Info() {
+  const [activePath, setActivePath] = useState("wedding-hall");
+  const navigate = useNavigate();
+
+  const handleNavClick = (path, state) => {
+    setActivePath(path);
+    navigate(`/info/${path}`, { state });
+  };
+
   return (
     <div>
       <nav className={classes.navContainer}>
-        <NavLink
-          to='/info/wedding-hall'
-          state={{ productType: "HALL", navAction: "info" }}
+        <div
+          className={`${classes.navItem} ${
+            activePath === "wedding-hall" ? classes.active : ""
+          }`}
+          onClick={() =>
+            handleNavClick("wedding-hall", {
+              productType: "HALL",
+              navAction: "info",
+            })
+          }
         >
-          <div>웨딩홀</div>
-        </NavLink>
-        <NavLink
-          to='/info/studio'
-          state={{ productType: "STUDIO", navAction: "info" }}
+          웨딩홀
+        </div>
+        <div
+          className={`${classes.navItem} ${
+            activePath === "studio" ? classes.active : ""
+          }`}
+          onClick={() =>
+            handleNavClick("studio", {
+              productType: "STUDIO",
+              navAction: "info",
+            })
+          }
         >
           <div>스튜디오</div>
-        </NavLink>
-        <NavLink
-          to='/info/dress'
-          state={{ productType: "DRESS", navAction: "info" }}
+        </div>
+        <div
+          className={`${classes.navItem} ${
+            activePath === "dress" ? classes.active : ""
+          }`}
+          onClick={() =>
+            handleNavClick("dress", {
+              productType: "HALL",
+              navAction: "info",
+            })
+          }
         >
           <div>드레스</div>
-        </NavLink>
-        <NavLink
-          to='/info/make-up'
-          state={{ productType: "MAKEUP", navAction: "info" }}
+        </div>
+        <div
+          className={`${classes.navItem} ${
+            activePath === "make-up" ? classes.active : ""
+          }`}
+          onClick={() =>
+            handleNavClick("make-up", {
+              productType: "HALL",
+              navAction: "info",
+            })
+          }
         >
           <div>메이크업</div>
-        </NavLink>
-        <NavLink
-          to='/info/mobile-invitation'
-          state={{ pageTitle: "모바일청첩장", navAction: "info" }}
+        </div>
+        <div
+          className={`${classes.navItem} ${
+            activePath === "mobile-invitation" ? classes.active : ""
+          }`}
+          onClick={() =>
+            handleNavClick("mobile-invitation", {
+              productType: "HALL",
+              navAction: "info",
+            })
+          }
         >
           <div>모바일청첩장</div>
-        </NavLink>
+        </div>
       </nav>
       <Outlet />
     </div>
