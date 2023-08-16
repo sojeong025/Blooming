0047fb121995c90fd3658831c2e6b672ab892f1c
김성인 <kimsungin5@gmail.com>
2023-08-16 15:05:03 +0900

    fix: 나의 찜이 아닌 찜 삭제할 때 잘못된 요청임 에러 내리기

--- backend/src/main/java/com/ssafy/backend/domain/wishlist/repository/WishlistRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/wishlist/repository/WishlistRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.domain.wishlist.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +12,8 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long>{
 //    @Query("select p from Product p join fetch ")
     List<Wishlist> findAllByUser(User user);
     void deleteByProductIdAndUserId(Long productId, Long userId);
+
+    Optional<Wishlist> findByProductIdAndUserId(Long productId, Long userId);
 
     void deleteAllByUserId(Long userId);
 
