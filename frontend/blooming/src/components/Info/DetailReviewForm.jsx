408b381c35a8e7196a053fb1d613cc64e519562e
정훈석 <wjdgnstjr45@naver.com>
2023-08-18 12:12:19 +0900

    fix: 주석 에러

--- frontend/blooming/src/components/Info/DetailReviewForm.jsx
+++ frontend/blooming/src/components/Info/DetailReviewForm.jsx
@@ -56,8 +56,8 @@ export default function DetailReviewForm({
         formData.append("image", file);
         const response = await fileAxios.post("REVIEW", formData);
         setImgFile(response.data.result[0].uploadImageUrl);
-      // } catch (error) {
-      //   console.error("이미지 api 오류", error);
+      } catch (error) {
+        // console.error("이미지 api 오류", error);
       }
       const reader = new FileReader();
       reader.onloadend = () => {
