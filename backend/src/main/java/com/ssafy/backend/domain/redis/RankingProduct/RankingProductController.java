c32032bd2b98b89380e76118e19a9d6f56cbbf3d
김성인 <kimsungin5@gmail.com>
2023-08-21 13:53:34 +0900

    feat: 성능을 보기 위한 시간 체크

--- backend/src/main/java/com/ssafy/backend/domain/redis/RankingProduct/RankingProductController.java
+++ backend/src/main/java/com/ssafy/backend/domain/redis/RankingProduct/RankingProductController.java
@@ -36,6 +36,8 @@ public class RankingProductController {
     @Operation(description = "예약 랭킹 상위 10개의 상품 조회")
     @GetMapping("/ranking/{productType}")
     public ResponseEntity<BasicResponse> getRanking(@PathVariable ProductType productType){
+        long startTime = System.currentTimeMillis();
+
         //랭킹 검색 : redis
         String key = "ranking:" + productType;
         Set<ZSetOperations.TypedTuple<String>> typedTuples = redisTemplate.opsForZSet().reverseRangeWithScores(key, 0, 9);
@@ -45,6 +47,11 @@ public class RankingProductController {
                 .collect(Collectors.toList());
 
         List<ProductRankingDto> productRankingDtoList = productRepository.getProductRankingInfo(productIds);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("REDIS 시작 : "+startTime);
+        System.out.println("REDIS 종료 : "+endTime);
+        System.out.println("REDIS 사용 시"+(endTime-startTime));
 
         BasicResponse basicResponse = BasicResponse.builder()
                                         .code(HttpStatus.OK.value())
@@ -59,12 +66,18 @@ public class RankingProductController {
     @Operation(description = "mysql 버전 예약 랭킹 상위 10개의 상품 조회")
     @GetMapping("/ranking/mysql/{productType}")
     public ResponseEntity<BasicResponse> getMysqlRanking(@PathVariable ProductType productType){
+        long startTime = System.currentTimeMillis();
         List<Product> productList = productRepository.findTop10ByProductTypeOrderByReservationCountDesc(productType);
 
         List<ProductRankingDto> productRankingDtoList = productList.stream()
                 .map(product -> new ProductRankingDto(product.getId(), product.getItemName(), product.getProductType(),
                         product.getBrief(), product.getCompany(), product.getThumbnail()))
                 .collect(Collectors.toList());
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("REDIS 시작 : "+startTime);
+        System.out.println("REDIS 종료 : "+endTime);
+        System.out.println("REDIS 사용 시"+(endTime-startTime));
 
         BasicResponse basicResponse = BasicResponse.builder()
                 .code(HttpStatus.OK.value())
